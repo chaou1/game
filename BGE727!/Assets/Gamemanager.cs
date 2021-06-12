1e15a7a485855a633461fd261a08b50fa8c8e073
Ruihaolol <ruihao111@gmail.com>
2021-06-12 15:59:46 +0200

    scene2

--- BGE727!/Assets/Gamemanager.cs
+++ BGE727!/Assets/Gamemanager.cs
@@ -3,18 +3,27 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Gamemanager : MonoBehaviour
+
+    
 {
     bool gameHasEnded = false;
     public void endGame() {
         if (gameHasEnded == false) {
             gameHasEnded = true;
-            restart();
+            Restart();
         
         }
     
     }
-    void restart() {
+    void Restart() {
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void NextScene()
+    {
+
+        SceneManager.LoadScene(1);
+
+
     }
 }
