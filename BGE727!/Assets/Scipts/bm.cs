7f15645cf049371a0ab7d9978191f658afb61d38
chaou1 <77403427+chaou1@users.noreply.github.com>
2021-06-12 12:55:17 +0200

    jumping

--- BGE727!/Assets/Scipts/bm.cs
+++ BGE727!/Assets/Scipts/bm.cs
@@ -8,6 +8,15 @@ public class bm : MonoBehaviour
 
 
 public float movementspeed;
+public float jumpstrength;
+public bool isGrounded = false;
+GameObject Player;
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,4 +29,20 @@ public float movementspeed;
           Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
         transform.position = transform.position + horizontal * Time.deltaTime * movementspeed;
     }
+
+
+    
+     void Jump()
+        {
+            if (Input.GetButton("Jump") && isGrounded == true
+                   )
+            {
+               
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpstrength), ForceMode2D.Impulse);
+
+             
+            }
+            Jump();
+
+        }
 }
---
+++ BGE727!/Assets/Scipts/gc.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gc : MonoBehaviour
+{
+   public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.collider.CompareTag("ground")) { Player.GetComponent<bm>().isGrounded = true; };
+
+    }
+
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "ground") { Player.GetComponent<bm>().isGrounded = false; }
+
+    }
+}
