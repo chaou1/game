559f6e85b4298eab7e9ff71cd67fdf7b8693ebb1
Ruihaolol <ruihao111@gmail.com>
2021-06-12 14:54:39 +0200

    gameover

--- BGE727!/Assets/Gamemanager.cs
+++ BGE727!/Assets/Gamemanager.cs
@@ -1,16 +1,21 @@
 
-using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Gamemanager : MonoBehaviour
 {
     bool gameHasEnded = false;
+    public float RestartDelay = 1f;
     public void endGame() {
         if (gameHasEnded == false) {
             gameHasEnded = true;
-
+            Invoke(restart, RestartDelay);
         
         }
+    
+    }
+    void restart() {
 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
